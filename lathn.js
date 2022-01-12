e3b319ac01a123d496d6daef5a8511bf9e7e7faf
Alifmslm <alifmslm01@gmail.com>
2022-01-12 11:04:37 +0700

    first commit

---
+++ lathn.js
@@ -0,0 +1,20 @@
+let firstname = "Alif ";
+let lastname = "Muslim ";
+let namawatashi = "Abdurrahman";
+let cek = "Status : "
+let b = "SMK "
+let c = "SMP "
+let a = "SD "
+let umur = 15;
+let h ="SMK";
+
+
+let yes = umur > 14
+let no = umur < 14
+
+
+console.log(firstname + lastname + namawatashi)
+console.log(cek)
+console.log(b + yes)
+console.log(c + no)
+console.log(a + no)
